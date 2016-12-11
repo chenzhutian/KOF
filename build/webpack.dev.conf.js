d6ca14d4c5ff866d34e3c472a0f67770539cd6dc
unHealthy <zhutian.chen@outlook.com>
2016-12-11 18:59:51 +0800

    add scalfolder

---
+++ build/webpack.dev.conf.js
@@ -0,0 +1,41 @@
+const config = require('../config')
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const utils = require('./utils')
+const baseWebpackConfig = require('./webpack.base.conf')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+// add hot-reload related code to entry chunks
+Object.keys(baseWebpackConfig.entry).forEach(function (name) {
+    baseWebpackConfig.entry[name].unshift('webpack-hot-middleware/client?noInfo=true&reload=true');
+})
+
+module.exports = merge(baseWebpackConfig, {
+    module: {
+        loaders: utils.styleLoaders({
+            sourceMap: config.dev.cssSourceMap
+        })
+    },
+    // eval-source-map is faster for development
+    devtool: '#eval-source-map',
+    output: {
+        path: config.dev.assetsRoot,
+        publicPath: config.dev.assetsPublicPath,
+        filename: '[name].js',
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': config.dev.env
+        }),
+        // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin(),
+        // https://github.com/ampedandwired/html-webpack-plugin
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'index.html',
+            inject: true
+        })
+    ]
+})
